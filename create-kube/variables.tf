bfb23ecef31a6127262dd4e493680809a321966b
rohitabraham02 <rohit.abraham02@gmail.com>
2022-07-16 04:07:34 +0530

    Create variables.tf

---
+++ create-kube/variables.tf
@@ -0,0 +1,16 @@
+variable "gcp_project_id" {
+  description = "project id"
+}
+
+variable "gcp_region" {
+  description = "region"
+}
+
+variable "gcp_network_name" {
+ description = "desc"
+}
+
+variable "gcp_sub_network" {
+ description =" subnetowrk"
+}
+
